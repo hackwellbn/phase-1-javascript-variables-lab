1a4b081165be90c420c23b32d915ff991b7992f7
hackwellbn <119762901+hackwellbn@users.noreply.github.com>
2025-03-08 16:38:50 +0000

    adss sliced

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 let companyName = 'elevated steel solutions'
 
 const greetingsUpdates = `${companyName.slice(0, 3)} can be sliced as well`
+
+
+
+function addData(sampleData){
+    if(sampleData < 10){
+        result =  sampleData + 1
+    }
+    return result
+
+}
+
+addData(20)
