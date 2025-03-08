5344f2840203464b4e1ab709e0f75b47e3b94599
hackwellbn <119762901+hackwellbn@users.noreply.github.com>
2025-03-08 16:42:43 +0000

    adds functions

--- index.js
+++ index.js
@@ -14,3 +14,35 @@ function addData(sampleData){
 }
 
 addData(20)
+
+
+function scuberGreetingForFeet(distance) {
+    if (distance <= 400) {
+      return "This will be a great ride!";
+    } else if (distance > 400 && distance <= 2000) {
+      return "That will be a big ride!";
+    } else if (distance > 2000 && distance <= 2500) {
+      return "I hope you have a wonderful trip!";
+    } else {
+      return "Sorry, we don't provide rides for this distance.";
+    }
+  }
+
+
+  function ternaryCheckCity(city) {
+    return city === "NYC" ? "Welcome to the Big Apple!" : "Sorry, we only serve NYC.";
+  }
+
+  
+  function switchOnCharmFromTip(tip) {
+    switch (tip) {
+      case "generous":
+        return "Thank you for your generosity!";
+      case "not generous":
+        return "Thank you for riding with us.";
+      default:
+        return "Have a nice day!";
+    }
+  }
+  
+  
