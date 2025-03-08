28441d6989af6179b82f7f53c35d737fc4493a60
hackwellbn <119762901+hackwellbn@users.noreply.github.com>
2025-03-08 16:49:51 +0000

    adds functions

--- index.js
+++ index.js
@@ -45,4 +45,18 @@ function scuberGreetingForFeet(distance) {
     }
   }
   
+
+  let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 20;
+let num4 = 6;
+let mod = num3 % num4;
+
+let numbers = [10, 15, 20, 5, 30];
+let max = Math.max(...numbers);
+
   
